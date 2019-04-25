81034a7b04e6279ad4c040e0a82188310d931bd0
Boxor <cdschonlau@hotmail.com>
2019-04-25 11:12:11 -0500

    Started some classwork

---
+++ Week 5/Day 22/resolvers-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TodosComponent } from './todos/todos.component';
+import { UsersComponent } from './users/users.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    TodosComponent,
+    UsersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
